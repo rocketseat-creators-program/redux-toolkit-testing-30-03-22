8a0239c321ce1cf78c2fd36fb3717a195c993f18
vitormalencar <vitor.maia.alencar@gmail.com>
2022-03-30 13:53:10 +0200

    add mock files

---
+++ src/mocks/results.ts
@@ -0,0 +1,37 @@
+export const resultMock = {
+  id: 1,
+  name: "Yolanda Little",
+  place: "South Tommiemouth",
+  distance: 63011,
+  price: 1000,
+  image:
+    "https://a0.muscache.com/im/pictures/be42241a-5346-4745-a2ef-8cf7576f88b8.jpg?im_w=720",
+  rating: 23968,
+  review: 6294,
+};
+
+const resultsMock2 = {
+  id: 2,
+  name: "Rafael Oberbrunner",
+  place: "Daytona Beach",
+  distance: 63011,
+  price: 78390,
+  image:
+    "https://a0.muscache.com/im/pictures/be42241a-5346-4745-a2ef-8cf7576f88b8.jpg?im_w=720",
+  rating: 23968,
+  review: 6294,
+};
+
+const resultsMock3 = {
+  id: 3,
+  name: "Rafael dit",
+  place: "Yolanda Beach",
+  distance: 63011,
+  price: 78390,
+  image:
+    "https://a0.muscache.com/im/pictures/be42241a-5346-4745-a2ef-8cf7576f88b8.jpg?im_w=720",
+  rating: 23968,
+  review: 6294,
+};
+
+export const resultsMock = [resultMock, resultsMock2, resultsMock3];
